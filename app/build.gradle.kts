9005e9ed1f1a58d292a90f64ae1076a095440d18
Cheva <super_chen@126.com>
2026-04-03 11:40:19 +0800

    chore: bump version to 1.1 and add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vrplayer.bilisbs"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
